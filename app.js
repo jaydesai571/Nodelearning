62947cddfbeaefc3fcb2c657f214440a90c73964
Jay Desai <jaydesai571@gmail.com>
2020-10-02 10:07:13 -0400

    View engine add EJS and sampled with profile view and data

--- app.js
+++ app.js
@@ -21,8 +21,9 @@
 
 
 var express = require('express');
-
 var app = express();
+
+app.set('view engine', 'ejs');
 
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/index.html');
@@ -33,8 +34,9 @@ app.get('/contact',function(req,res) {
     res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id',function(req,res) {
-    res.send('you requsted to see a profile with the id: ' + req.params.id);
+app.get('/profile/:name',function(req,res) {
+    var data ={age:29, job: 'ninja'}
+    res.render('profile',{person: req.params.name, data: data});
 });
 
 app.listen(3000);
