467601b23ba9c14ae3f48a95b07db202692d3c65
Jay Desai <jaydesai571@gmail.com>
2020-10-02 09:51:25 -0400

    How to add html pages to node through express

--- app.js
+++ app.js
@@ -25,16 +25,16 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-    res.send('this is home page');
+    res.sendFile(__dirname + '/index.html');
 });
 
 
 app.get('/contact',function(req,res) {
-    res.send('this is contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:id',function(req,res) {
-    res.send('you requsted to see a profile with the id:' + req.params.id);  
+    res.send('you requsted to see a profile with the id: ' + req.params.id);
 });
 
 app.listen(3000);
