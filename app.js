21b6d93e3adc3adb4ce9c44ad221d6904eb7af60
Jay Desai <jaydesai571@gmail.com>
2020-10-02 10:29:45 -0400

    Added partials view through Express.js

--- app.js
+++ app.js
@@ -26,16 +26,18 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 
 app.get('/contact',function(req,res) {
-    res.sendFile(__dirname + '/contact.html');
+    // res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 app.get('/profile/:name',function(req,res) {
-    var data ={age:29, job: 'ninja'}
+    var data ={age:29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']}
     res.render('profile',{person: req.params.name, data: data});
 });
 
