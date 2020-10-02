fc9128e3e7a63c6999cd267a721323a433b1f374
Jay Desai <jaydesai571@gmail.com>
2020-10-02 01:25:37 -0400

    Added express as dependenices and made package.json

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
+//
+// var server = http.createServer(function(req, res) {
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+//     // myReadStream.pipe(res);
+//
+//     var myObj = {
+//       name: 'Ryu',
+//       job: ' Ninja',
+//       age: '29'
+//     };
+//     res.end(JSON.stringify(myObj));
+// });
+//
+// server.listen(3000, '127.0.0.1');
+// console.log('yo Dawgs, now listening to port 3000 ');
+// // pipe is only for read stream
 
-var server = http.createServer(function(req, res) {
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    // myReadStream.pipe(res);
 
-    var myObj = {
-      name: 'Ryu',
-      job: ' Ninja',
-      age: '29'
-    };
-    res.end(JSON.stringify(myObj));
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res) {
+    res.send('this is home page');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo Dawgs, now listening to port 3000 ');
-// pipe is only for read stream
+
+app.get('/contact',function(req,res) {
+    res.send('this is contact page');
+});
+
+app.get('/profile/:id',function(req,res) {
+    res.send('you requsted to see a profile with the id:' + req.params.id);  
+});
+
+app.listen(3000);
